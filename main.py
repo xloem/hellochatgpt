cc968ca4d39df3c01a64aed21042d293f861879f
John Doe <johndoe@example.com>
2022-12-24 08:00:47 -0500

    03

--- main.py
+++ main.py
@@ -1,6 +1,23 @@
-import tkinter as tk
+import random
+import time
 
-root = tk.Tk()
-label = tk.Label(root, text="Hello, World!")
-label.pack()
-root.mainloop()
+def calm(num_minutes):
+    """
+    Display calming messages and breathe in and out for a specified number of minutes.
+    """
+    messages = [
+        "Take a deep breath in...",
+        "Hold it for a moment...",
+        "And slowly exhale...",
+        "Close your eyes and focus on your breath...",
+        "Let go of any tension in your body...",
+        "Allow yourself to feel relaxed and calm...",
+        "You are doing great!"
+    ]
+    
+    end_time = time.time() + 60 * num_minutes
+    while time.time() < end_time:
+        print(random.choice(messages))
+        time.sleep(5)
+
+calm(10)
