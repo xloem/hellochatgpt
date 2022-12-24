cabf67814fe524c022afca573241a250d598a44e
John Doe <johndoe@example.com>
2022-12-24 08:14:30 -0500

    -

--- calm.py
+++ calm.py
@@ -9,12 +9,15 @@ def calm(num_minutes, standing):
     Display calming messages, guide the user through deep breathing exercises, and suggest standing up and moving around for a specified number of minutes.
     """
     end_time = time.time() + 60 * num_minutes
+    breathing_time = 5
     while time.time() < end_time:
         print(breathing_messages.get_inhale_message())
-        timer.time_breath(10)
+        timer.time_breath(breathing_time)
         print(breathing_messages.get_exhale_message())
-        timer.time_breath(10)
+        timer.time_breath(breathing_time)
         if standing:
             print(standing_messages.get_exercise_or_progress_message())
         else:
             print(standing_messages.get_stand_up_message())
+        # Increase the breathing time by 1 second for each iteration
+        breathing_time += 1
